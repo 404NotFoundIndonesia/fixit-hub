9336be46aeaf68ae4398d7485d6003154c0ae4d2
M. Iqbal Effendi <iqbaleff214@gmail.com>
2026-05-13 15:56:31 +0800

    style: apply airbnb theme

--- app/javascript/controllers/message_controller.js
+++ app/javascript/controllers/message_controller.js
@@ -4,12 +4,24 @@ export default class extends Controller {
   static targets = ["list", "input"]
 
   connect() {
+    this.currentUserId = document.body.dataset.currentUserId
+    this.applyMineTheirs()
     this.scrollToBottom()
   }
 
-  // Called whenever Turbo appends a new message to the list
   listTargetConnected() {
+    this.applyMineTheirs()
     this.scrollToBottom()
+  }
+
+  applyMineTheirs() {
+    if (!this.hasListTarget || !this.currentUserId) return
+    this.listTarget.querySelectorAll(".message[data-sender-id]").forEach(el => {
+      el.classList.remove("message--mine", "message--theirs")
+      el.classList.add(
+        el.dataset.senderId === this.currentUserId ? "message--mine" : "message--theirs"
+      )
+    })
   }
 
   scrollToBottom() {
